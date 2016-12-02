ffadf609ba5ac301426b9332a14af24a11b1e809
Donovan Tengblad <purplefish32@gmail.com>
2016-12-02 14:39:58 +0100

    Test

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -2,29 +2,6 @@
 
 set -e
 
-echo **************hello******************
-
-echo 'Checking for existing certificates'
-if [ ! -f "/certs/haproxy/cert.pem" ]; then
-  echo 'No certificates found matching your app url'
-  echo 'Generating self-signed SSL certificates.'
-  openssl \
-  req -new -newkey rsa:4096 -days 365 -nodes -x509 \
-  -subj "/C=AU/ST=NSW/L=Sydney/O=My Org/CN=$APP_URL" \
-  -keyout "/etc/ssl/certs/key.pem" \
-  -out "/etc/ssl/certs/ca.pem"
-
-  cp /etc/ssl/certs/key.pem /certs/key.pem
-  cp /etc/ssl/certs/ca.pem /certs/ca.pem
-
-  mkdir /certs/haproxy/
-
-  cp /etc/ssl/certs/key.pem /certs/haproxy/cert.pem
-  cat /etc/ssl/certs/ca.pem >> /certs/haproxy/cert.pem
-else
-  echo 'Certificates found'
-fi
-
 echo 'Checking for claroline install'
 if [ ! -d "/var/www/html/claroline/web" ]; then
   echo 'No previous Claroline Connect install found, Installing Claroline Connect...'
@@ -96,11 +73,6 @@ else
   echo 'Claroline Connect found, no need to install it again.'
 fi
 
-
 #exit 1
-
-
-
-echo ***************bye*******************
 
 exec "$@"
